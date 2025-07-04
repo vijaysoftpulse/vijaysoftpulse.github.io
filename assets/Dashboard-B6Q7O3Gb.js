79acd6bad00c7fa6e99cdb1c84b19f289a053d71
Vijay Softpulse <softpulsedev28@gmail.com>
2025-07-04 17:42:06 +0530

    Deploy production build

---
+++ assets/Dashboard-B6Q7O3Gb.js
@@ -0,0 +1,1 @@
+import{j as a,P as s}from"./index-DToOBi_g.js";const r=()=>a.jsx(s,{title:"Dashboard"});export{r as default};
---
+++ assets/Settings-DFfHsswS.js
@@ -0,0 +1,1 @@
+import{j as t,P as s}from"./index-DToOBi_g.js";const e=()=>t.jsx(s,{title:"Dashboard"});export{e as default};
